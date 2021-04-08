2a09f09d498cadaeb672170332236cd0fa76a995
TCG <ssibonokuhle@gmail.com>
2021-04-08 08:58:21 +0200

    Created a store that will bring together the state, actions, and reducers that that make my app

--- src/redux/store/Index.js
+++ src/redux/store/Index.js
@@ -2,5 +2,6 @@ import { createStore } from "redux";
 import { rootReducer } from "../rootreducer";
 
 export const store = createStore(
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
