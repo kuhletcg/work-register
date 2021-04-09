ae97ef903b59355f87805d6bb095cfd9a8edd142
TCG <ssibonokuhle@gmail.com>
2021-04-09 09:24:00 +0200

    Made changes of inputs

--- src/App.js
+++ src/App.js
@@ -4,67 +4,137 @@ import { useSelector, useDispatch } from "react-redux";
 import { addForm, deleteForm } from "./redux/actions/Form";
 
 function App() {
-  const FormList = useSelector((state) => state.Form.FormList);
-  console.log("Formlist", FormList);
-  const dispatch = useDispatch();
-  const [Form, setForm] = useState("");
+  const [formInfo, setFormInfo] = useState({
+    name: "",
+    surname: "",
+    address: "",
+    email: "",
+    car: "",
+  });
+  const [saveInfo, setSaveInfo] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(addForm(Form));
-    setForm("");
-  };
+  function handleChange(event) {
+    setFormInfo({ ...formInfo, [event.target.name]: event.target.value });
+  }
 
-  const handleDelete = (key) => {
-    return FormList.filter((list) => list.key !== key);
-  };
+  function handleSubmit(event) {
+    const { name, surname, address, email, car } = formInfo;
+    event.preventDefault();
+    const newInfo = { ...formInfo, name, surname, address, email, car };
+    setSaveInfo([...saveInfo, newInfo]);
+    console.log(saveInfo);
+  }
 
   return (
-    <div className="App">
-      <h1>Work register</h1>
+    <div className="wrapper">
+      <h2>Work register</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          Name:
+        <label>
+          <p>Name</p>
           <input
-            type="FormRegisterapp"
-            onChange={(e) => setForm(e.target.value)}
-            placeholder=""
-            value={Form}
+            type="text"
+            name="name"
+            value={formInfo.name}
+            onChange={handleChange}
           />
-          Surname:
+        </label>
+        <label>
+          <p>Surname</p>
           <input
-            type="FormRegisterapp"
-            onChange={(e) => setForm(e.target.value)}
-            placeholder=""
-            value={Form}
+            type="text"
+            name="surname"
+            value={formInfo.surname}
+            onChange={handleChange}
           />
-          Work:
+        </label>
+        <label>
+          <p>Work</p>
           <input
-            type="FormRegisterapp"
-            onChange={(e) => setForm(e.target.value)}
-            placeholder=""
-            value={Form}
+            type="address"
+            name="address"
+            value={formInfo.address}
+            onChange={handleChange}
           />
-          <input type="button" value="Submit" onClick={handleSubmit} />
-        </div>
+        </label>
+        <button type="submit">Submit</button>
       </form>
-      {FormList &&
-        FormList.map((Form) => {
-          return (
-            <div key={Form.key}>
-              <h2>
-                <button
-                  onClick={() => dispatch(deleteForm(handleDelete(Form.key)))}
-                >
-                  delete
-                </button>
-              </h2>
-              <h1>{Form.name}</h1>
-            </div>
-          );
-        })}
+
+      {saveInfo.map((info) => {
+        return (
+          <div>
+            <h2>{info.name}</h2>
+            <h2>{info.surname}</h2>
+            <h2>{info.address}</h2>
+            <h2>{info.email}</h2>
+            <h2>{info.car}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
+// function App() {
+//   const FormList = useSelector((state) => state.Form.FormList);
+//   console.log("Formlist", FormList);
+//   const dispatch = useDispatch();
+//   const [Form, setForm] = useState("");
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     dispatch(addForm(Form));
+//     setForm("");
+//   };
+
+//   const handleDelete = (key) => {
+//     return FormList.filter((list) => list.key !== key);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Work register</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           Name:
+//           <input
+//             type="FormRegisterapp"
+//             onChange={(e) => setForm(e.target.value)}
+//             placeholder=""
+//             value={Form}
+//           />
+//           Surname:
+//           <input
+//             type="FormRegisterapp"
+//             onChange={(e) => setForm(e.target.value)}
+//             placeholder=""
+//             value={Form}
+//           />
+//           Work:
+//           <input
+//             type="FormRegisterapp"
+//             onChange={(e) => setForm(e.target.value)}
+//             placeholder=""
+//             value={Form}
+//           />
+//           <input type="button" value="Submit" onClick={handleSubmit} />
+//         </div>
+//       </form>
+//       {FormList &&
+//         FormList.map((Form) => {
+//           return (
+//             <div key={Form.key}>
+//               <h2>
+//                 <button
+//                   onClick={() => dispatch(deleteForm(handleDelete(Form.key)))}
+//                 >
+//                   delete
+//                 </button>
+//               </h2>
+//               <h1>{Form.name}</h1>
+//             </div>
+//           );
+//         })}
+//     </div>
+//   );
+// }
 
 export default App;
