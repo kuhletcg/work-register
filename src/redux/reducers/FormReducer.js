bab5e507811a05a251b132e1793e894df46de3e6
TCG <ssibonokuhle@gmail.com>
2021-04-21 09:57:39 +0200

    Imported edit button

--- src/redux/reducers/FormReducer.js
+++ src/redux/reducers/FormReducer.js
@@ -1,33 +1,48 @@
-import { ADD_FORM, DELETE_FORM, EDIT_FORM } from "../actionTypes/Form";
+import {
+  ADD_FORM,
+  DELETE_FORM,
+  EDIT_FORM,
+  ADD_FORMLIST,
+  COMPLETE_FORM,
+} from "../actionTypes/Form";
 
 const initialState = {
-  formList: [],
+  FormList: [],
 };
 
 const FormReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_FORM:
+      console.log("name", action.payload);
       return {
         ...state,
-        formList: [
-          ...state.formList,
+        FormList: [
+          ...state.FormList,
           {
             key: Math.random(),
             name: action.payload,
-            surname: action.payload,
-            work: action.payload,
           },
         ],
       };
     case DELETE_FORM:
       return {
         ...state,
-        formList: action.payload,
+        FormList: action.payload,
       };
     case EDIT_FORM:
       return {
         ...state,
-        formList: action.payload,
+        FormList: action.payload,
+      };
+    case ADD_FORMLIST:
+      return {
+        ...state,
+        FormList: action.payload,
+      };
+    case COMPLETE_FORM:
+      return {
+        ...state,
+        FormList: action.payload,
       };
     default:
       return state;
