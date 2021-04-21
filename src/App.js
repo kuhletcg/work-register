1c02e40674fcb4dd2ffb3727b52e3b73a917134d
TCG <ssibonokuhle@gmail.com>
2021-04-21 09:54:10 +0200

    added edit button

--- src/App.js
+++ src/App.js
@@ -1,73 +1,118 @@
 import React, { useState } from "react";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-import { addForm } from "./redux/actions/Form";
+import {
+  addForm,
+  deleteForm,
+  editForm,
+  completeForm,
+} from "./redux/actions/Form";
 
 function App() {
   const FormList = useSelector((state) => state.Form.FormList);
-  console.log("formlist", FormList);
+  console.log("FormList", FormList);
   const dispatch = useDispatch();
-  const [form, setForm] = useState({ name: "", surname: "", work: "" });
-  console.log(form);
+  const [Form, setForm] = useState({ name: "", surname: "", work: "" });
+  console.log(Form);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addForm(form));
+    dispatch(addForm(Form));
     setForm({ name: "", surname: "", work: "" });
   };
 
   const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+    setForm({ ...Form, [e.target.name]: e.target.value });
+  };
+
+  const handleDelete = (key) => {
+    return FormList.filter((list) => list.key !== key);
+  };
+
+  const handleEdit = (key) => {
+    return FormList.filter((list) => list.key !== key);
+    //   var { selectedEdit } = this.state
+    //   this.setState({ selectedEdit: { index: selectedEdit.index, work: e.target.value}})
+  };
+
+  const handleComplete = (key) => {
+    return FormList.filter((list) => list.key !== key);
   };
 
   return (
-    <div className="Form">
+    <div className="form">
       <h2>Work register</h2>
-      <form className="Form" onSubmit={(e) => handleSubmit(e)}>
-        <label>
-          <p>Name</p>
-          <input
-            type="name"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          <p>Surname</p>
-          <input
-            type="surname"
-            name="surname"
-            value={form.surname}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          <p>Work</p>
-          <input
-            type="work"
-            name="work"
-            value={form.work}
-            onChange={handleChange}
-          />
-        </label>
-        <button onClick="submit">Submit</button>
-      </form>
+      <form onSubmit={handleSubmit}>
+        <p>Name</p>
+        <input
+          type="name"
+          name="name"
+          value={Form.name}
+          onChange={handleChange}
+          required
+        />
 
-      {FormList &&
-        FormList.map((form) => {
-          return (
-            <div key={form.key}>
-              <table>
-                <tr>
-                  <th>{form.name}</th>
-                  <th>{form.surname}</th>
-                  <th>{form.work}</th>
-                </tr>
-              </table>
-            </div>
-          );
-        })}
+        <p>Surname</p>
+        <input
+          type="surname"
+          name="surname"
+          value={Form.surname}
+          onChange={handleChange}
+          required
+        />
+
+        <p>Work</p>
+        <input
+          type="work"
+          name="work"
+          value={Form.work}
+          onChange={handleChange}
+          required
+        />
+        <div>
+          {/* <input value={Form.work} onChange={handleChange} /> */}
+          <button onClick={() => dispatch(editForm(handleEdit(Form.key)))}>
+            Edit
+          </button>
+
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>surname</th>
+            <th>work</th>
+          </tr>
+          <tbody>
+            {FormList &&
+              FormList.map((form) => {
+                return (
+                  <tr key={form.key}>
+                    <td>{form.name.name}</td>
+                    <td>{form.name.surname}</td>
+                    <td>{form.name.work}</td>
+                    <button
+                      onClick={() =>
+                        dispatch(completeForm(handleComplete(form.key)))
+                      }
+                    >
+                      Complete{" "}
+                    </button>
+                    <button
+                      onClick={() =>
+                        dispatch(deleteForm(handleDelete(form.key)))
+                      }
+                    >
+                      delete{" "}
+                    </button>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </thead>
+      </table>
     </div>
   );
 }
