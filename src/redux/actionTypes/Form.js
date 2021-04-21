83de4dd5502244b77644429bed5394a0790c695b
TCG <ssibonokuhle@gmail.com>
2021-04-21 09:55:24 +0200

    Exported edit button and complete form

--- src/redux/actionTypes/Form.js
+++ src/redux/actionTypes/Form.js
@@ -1,3 +1,5 @@
 export const ADD_FORM = "ADD_FORM";
 export const DELETE_FORM = "DELETE_FORM";
 export const EDIT_FORM = "EDIT_FORM";
+export const ADD_FORMLIST = "ADD_FORMLIST";
+export const COMPLETE_FORM = "COMPLETE_FORM";
